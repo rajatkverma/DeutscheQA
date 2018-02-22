a10df4bf19cacad2992dc82e9915dfcff1cd429b
Ahmad Sakor <sakerobs@gmail.com>
2018-02-22 10:32:24 +0100

    update labels creator

--- src/germanLabelsCreator.py
+++ src/germanLabelsCreator.py
@@ -8,6 +8,7 @@ import requests
 import io
 import csv
 from SPARQLWrapper import SPARQLWrapper, JSON
+import time
 
 def get_DBPedia_DE_label(uri,language):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -24,47 +25,6 @@ def get_DBPedia_DE_label(uri,language):
         labels.append(result['label']['value'])
     return labels
 
-def get_WikiData_Id(uri):
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    sparql.setQuery("""
-    SELECT DISTINCT ?wiki
-    WHERE {
-            <"""+uri+"""> <http://www.w3.org/2002/07/owl#sameAs> ?wiki.
-            FILTER regex(?wiki,'wikidata.org')
-    }
-    """)
-    sparql.setReturnFormat(JSON)
-    sparql.setTimeout(10000)
-    results = sparql.query().convert()
-    try:
-        wid = results["results"]["bindings"][0]['wiki']['value']
-        return wid[wid.rfind('/')+1:]
-    except IndexError:
-        return ''
-
-def get_WikiData_DE_label(wikiId):
-    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-    sparql.setQuery("""PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX wd: <http://www.wikidata.org/entity/>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX dbo: <http://dbpedia.org/ontology/>
-    SELECT DISTINCT ?label 
-    WHERE 
-    { 
-      {wd:"""+wikiId+""" skos:altLabel ?label.} 
-      UNION
-      {wd:"""+wikiId+""" rdfs:label ?label.} 
-      UNION
-      {wd:"""+wikiId+""" dbo:alias ?label.}
-      FILTER (lang(?label) = 'en')
-    } """)
-    sparql.setReturnFormat(JSON)
-    sparql.setTimeout(10000)
-    results = sparql.query().convert()
-    labels = []
-    for result in results['results']['bindings']:
-        labels.append(result['label']['value'])
-    return labels
 
 def get_synonyms_wordnet(word,language):
     app_id = ''
@@ -86,10 +46,14 @@ def get_synonyms_wordnet(word,language):
 
 if __name__ == '__main__':
     uriLabels=dict({})
-    with open("labels.csv", "w") as out:
+    request=0
+    t0 = time.time()
+    with open("labels.csv", "a+") as out:
         writer = csv.writer(out)
         with io.open('./dbpredicateindex14.json', mode="r", encoding="utf-8") as f:
             for cnt, line in enumerate(f):
+                #if cnt < 20907:
+                    #continue
                 obj = json.loads(line)
                 url = obj['_source']['uri']
                 if url in uriLabels:
@@ -97,10 +61,30 @@ if __name__ == '__main__':
                     continue
                 #url = 'http://dbpedia.org/resource/Cologne'
                 print(cnt,url)
-                DBlabels = get_DBPedia_DE_label(url,'en')
+                DBlabels = get_DBPedia_DE_label(url,'de')
                 labelsSet=list(set(DBlabels))
-                synonyms=get_synonyms_wordnet(url[url.rfind('/')+1:],'en')
-                labelsSet=labelsSet+synonyms
-                uriLabels[url]=labelsSet
+                NewlabelsSet=labelsSet
+                for label in labelsSet:
+                    '''if request >=500:
+                        request=0
+                        t1 = time.time()
+                        total = t1-t0
+                        print ("request = ",request)
+                        print ("total = ",total)
+                        if total < 60:
+                            time.sleep(60-total)
+                        t0 = time.time()'''
+                    synonyms=get_synonyms_wordnet(label,'de')
+                    request=request+1
+                    print ("request = ",request)
+                    NewlabelsSet=NewlabelsSet+synonyms
+                    
+                    
+                synonyms=get_synonyms_wordnet(url[url.rfind('/')+1:],'de')
+                request=request+1
+                NewlabelsSet=NewlabelsSet+synonyms
+                NewlabelsSet=list(set(NewlabelsSet))
+                
+                uriLabels[url]=NewlabelsSet
                 
                 writer.writerow([url.encode('utf-8'),uriLabels[url]])
